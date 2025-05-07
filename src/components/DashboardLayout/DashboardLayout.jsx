5076c60e20d71d5b05080a5b11f8beefd0a25c50
devDawson <dauxonphilip@gmail.com>
2025-05-07 17:10:16 +0300

    Made multiple updates across the site

--- src/components/DashboardLayout/DashboardLayout.jsx
+++ src/components/DashboardLayout/DashboardLayout.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import NAvbar from '../DashboardComponents/NAvbar'
 
 const DashboardLayout = ({children}) => {
   return (
     <div>
-        <NAvbar/>
         {children}
     </div>
   )
